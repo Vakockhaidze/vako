fa1d232c391ea50e6f02940ddb0f32e98cea5144
Vakockhaidze <139356496+Vakockhaidze@users.noreply.github.com>
2023-08-18 23:14:58 +0400

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,7 @@
+name = ["ჩ", "ხ", "ა", "ი", "ძ", "ე", ]
+print(name[1] + name[3] + name[5] + name[7] + name[10] + name[12])
+
+family = ['ვაკო', 'ქეთი', 'თამთა', "ზაზა", "გულიკო"]
+print(family[0] + " " + family[3])
+print(family[1] + " " + family[3])
+print(family[2] + " " + family[4])
